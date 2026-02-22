65df9f19fd36671d0b3287f95c1f4578f0d9415b
ren-lingyu <Ren_Lingyu@outlook.com>
2026-02-22 18:19:16 +0000

    Refactor WSL host module layout and enable Ollama service
    - 调整模块组织顺序, 合并了一些属性的子属性 (如 `programs`, `users`).
    - 新增 `services.ollama` 配置, 启用 `pkgs.ollama-vulkan`, 绑定 `127.0.0.1:11434`, 并预加载 `phi3.5:3.8b-mini-instruct-q4_K_S`.
    - 将 `programs.dconf.enable` 并入 `programs` 集合, 同时把 `environment.variables.EDITOR` 收敛到 `environment.variables` 子属性.

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -31,65 +31,15 @@
     useWindowsDriver = true;
   };
 
-  users.groups.docker = {};
-  users.users.lingyu = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
-    home = "/home/lingyu";
-  };
-  
-  services = {
-    dbus = {
-      enable = true;
-      implementation = "broker";
+  users = {
+    groups.docker = {};
+    users.lingyu = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" ];
+      home = "/home/lingyu";
     };
-    vscode-server = {
-      enable = true;
-      enableFHS = true;
-    };
-  };
+  }; 
 
-  programs.nix-ld.enable = true;
-
-  environment = {
-    usrbinenv = lib.mkForce "${pkgs.coreutils}/bin/env";
-    binsh = "${pkgs.bash}/bin/sh";
-  };
-
-  systemd = {
-    services = {
-      "user@" = {
-        overrideStrategy = "asDropin";
-        serviceConfig = {
-          Delegate = "no";
-          DelegateSubgroup = "";
-        };
-      };
-    };
-    tmpfiles.rules = [
-      "f /var/lib/systemd/linger/lingyu 0644 root root -"
-    ];
-  };
-
-  programs.dconf.enable = true;
-  
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      qt6Packages.fcitx5-chinese-addons
-      fcitx5-rime
-    ];
-  };
-  
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    wqy_zenhei
-  ];
-  
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
@@ -128,8 +78,70 @@
     pinentry-tty
     qt6Packages.fcitx5-configtool
   ];
-  
-  environment.variables = {
-    EDITOR = "vim";
+    
+  services = {
+    dbus = {
+      enable = true;
+      implementation = "broker";
+    };
+    vscode-server = {
+      enable = true;
+      enableFHS = true;
+    };
+    ollama = {
+      enable = true;
+      package = pkgs.ollama-vulkan;
+      host = "127.0.0.1";
+      port = 11434; 
+      loadModels = [
+        "phi3.5:3.8b-mini-instruct-q4_K_S"
+      ];
+      syncModels = true;
+    };
   };
+
+  programs = {
+    nix-ld.enable = true;
+    dconf.enable = true;
+  }; 
+
+  environment = {
+    usrbinenv = lib.mkForce "${pkgs.coreutils}/bin/env";
+    binsh = "${pkgs.bash}/bin/sh";
+    variables = {
+      EDITOR = "vim";
+    }; 
+  };
+
+  systemd = {
+    services = {
+      "user@" = {
+        overrideStrategy = "asDropin";
+        serviceConfig = {
+          Delegate = "no";
+          DelegateSubgroup = "";
+        };
+      };
+    };
+    tmpfiles.rules = [
+      "f /var/lib/systemd/linger/lingyu 0644 root root -"
+    ];
+  };
+  
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      qt6Packages.fcitx5-chinese-addons
+      fcitx5-rime
+    ];
+  };
+  
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    wqy_zenhei
+  ];
 }
