0bf13f64278f6ded760b103755e712c2e2a90b2d
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-09 01:17:17 +0800

    Modified `systemd.mounts` and `systemd.tmpfiles.rules`
    将各种类似 Documents 的目录, 从 Windows 所在分区挂载到 NixOS 所在分区, 此外还添加了一些变量用于复用某些路径.

--- hosts/thinkbook/default.nix
+++ hosts/thinkbook/default.nix
@@ -4,6 +4,21 @@
 
 { config, lib, pkgs, ... }:
 
+let
+
+  bindfsMountOptions = lib.concatStringsSep "," [
+    "force-user=lingyu"
+    "force-group=users"
+    "perms=u=rwX:g=rX:o="
+    "create-as-user"
+    "chown-ignore"
+    "chgrp-ignore"
+  ];
+  nixosUserHome = "/home/lingyu";
+  windowsUserHome = "/mnt/c/Users/Lingyu";
+
+in
+
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -49,9 +64,79 @@
         hashedPassword = "$y$j9T$HZvnP.0ZR5uBiDAviT9xA.$MSExGgePZwjIDZq2n3fOUGGguWKEgvjuIKImW4uf7p4";
         linger = true;
         extraGroups = [ "wheel" "video" "render" ];
-	      home = "/home/lingyu";	
+	      home = nixosUserHome;
       };
     };
+  };
+
+  systemd = {
+    tmpfiles = {
+      rules = [
+        "d /mnt/c 0700 root root - -"
+        "d ${nixosUserHome}/knowhub 0755 lingyu users - -"
+        "d ${nixosUserHome}/Downloads 0755 lingyu users - -"
+        "d ${nixosUserHome}/Documents 0755 lingyu users - -"
+        "d ${nixosUserHome}/Pictures 0755 lingyu users - -"
+        "d ${nixosUserHome}/Videos 0755 lingyu users - -"
+        "d ${nixosUserHome}/Music 0755 lingyu users - -"
+      ];
+    };
+    mounts = [
+      {
+        what = "${windowsUserHome}/KnowledgeHub";
+        where = "${nixosUserHome}/knowhub";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+      {
+        what = "${windowsUserHome}/Downloads";
+        where = "${nixosUserHome}/Downloads";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+      {
+        what = "${windowsUserHome}/OneDrive/Documents";
+        where = "${nixosUserHome}/Documents";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+      {
+        what = "${windowsUserHome}/OneDrive/Pictures";
+        where = "${nixosUserHome}/Pictures";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+      {
+        what = "${windowsUserHome}/OneDrive/Videos";
+        where = "${nixosUserHome}/Videos";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+      {
+        what = "${windowsUserHome}/OneDrive/Music";
+        where = "${nixosUserHome}/Music";
+        type = "fuse.bindfs";
+        options = bindfsMountOptions;
+        wantedBy = [ "multi-user.target" ];
+        after = [ "mnt-c.mount" ];
+        requires = [ "mnt-c.mount" ];
+      }
+    ];
   };
 
   i18n = {
@@ -129,21 +214,6 @@
       XMODIFIERS = "@im=ibus";
       QT_IM_MODULE = "ibus";
     };
-  };
-
-
-  systemd = {
-    mounts = [
-      {
-        what = "/mnt/c/Users/Lingyu/KnowledgeHub";
-        where = "/home/lingyu/knowhub";
-        type = "fuse.bindfs";
-        options = "force-user=lingyu,force-group=100,perms=u=rwX:g=rX:o=,create-as-user,chown-ignore,chgrp-ignore";
-        wantedBy = [ "multi-user.target" ];
-        after = [ "mnt-c.mount" ];
-        requires = [ "mnt-c.mount" ];
-      }
-    ];
   };
 
   programs = {
