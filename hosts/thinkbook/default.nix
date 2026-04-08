c9f5af8529ce0ae79ce1e4ade73fa9e4d2edaa18
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-08 16:27:52 +0800

    Enable `i18n.inputMethod`

--- hosts/thinkbook/default.nix
+++ hosts/thinkbook/default.nix
@@ -56,7 +56,7 @@
       "de_DE.UTF-8/UTF-8"
     ];
     inputMethod = {
-      enable = false;
+      enable = true;
       type = "ibus";
       ibus.engines = with pkgs.ibus-engines; [
         libpinyin
