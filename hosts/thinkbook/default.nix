df89005a4994021eddf9cbf023a9094360af75cc
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-09 17:55:00 +0800

    Rename `hosts/thinkbook/desktop-manager.nix` to `hosts/thinkbook/desktop-environment.nix`

--- hosts/thinkbook/default.nix
+++ hosts/thinkbook/default.nix
@@ -8,7 +8,7 @@
   
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./desktop-manager.nix
+    ./desktop-environment.nix
     ./mount-windows-path.nix
   ];
 
