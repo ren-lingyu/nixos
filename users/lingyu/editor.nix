ffda1b766bf1589c60a31029190c537aac2664fa
ren-lingyu <Ren_Lingyu@outlook.com>
2026-04-13 01:50:21 +0800

    Extend nixvim configuration with aliases, options, and plugins in `users/lingyu/editor.nix`.
    - 为 nixvim 启用 viAlias 和 vimAlias 设置.
    - 设置行号缩进, 鼠标, 搜索, 颜色和剪贴板等选项.
    - 添加了包括 lsp 在内的 nvim 插件.
    - 使用 catppuccin 主题，并设置 flavour 为 "mocha".

--- users/lingyu/editor.nix
+++ users/lingyu/editor.nix
@@ -7,7 +7,62 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
     vimdiffAlias = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      expandtab = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      softtabstop = 2;
+      mouse = "a";
+      ignorecase = true;
+      smartcase = true;
+      termguicolors = true;
+      clipboard = "unnamedplus";
+    };
+    plugins = {
+      lualine.enable = true;
+      gitsigns.enable = true;
+      which-key.enable = true;
+      telescope.enable = true;
+      web-devicons.enable = true;
+      mini-icons.enable = true;
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+      };
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          nixd.enable = true;
+          bashls.enable = true;
+          clangd.enable = true;
+          gopls.enable = true;
+          jdtls.enable = true;
+          ts_ls.enable = true;
+          pyright.enable = true;
+          lua_ls.enable = true;
+          marksman.enable = true;
+          yamlls.enable = true;
+        };
+      };
+    };
+    colorschemes = {
+      catppuccin = {
+        enable = true;
+        settings.flavour = "mocha";
+      };
+    };
   };
 
   programs.vim = {
