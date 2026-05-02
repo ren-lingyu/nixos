bd2edf2db4870a3c15e9e773b1b9d12191d1a957
ren-lingyu <Ren_Lingyu@outlook.com>
2026-05-03 02:54:01 +0800

    chore(thinkbook): explicitly specify user name and uid
    - Modified: `modules/hosts/thinkbook/default.nix`.
    - 为 `root` 用户添加 `name = "root"`.
    - 为 `lingyu` 用户添加 `name = "lingyu"` 和 `uid = 1000`.

--- modules/hosts/thinkbook/default.nix
+++ modules/hosts/thinkbook/default.nix
@@ -72,9 +72,11 @@
     mutableUsers = false;
     users = {
       root = {
+        name = "root";
         hashedPassword = "$y$j9T$I75rvRCS.FR8zj/o3ivGb1$lteEgidoSR8AbjTXhxfemLeiXFIQsxUuUmSl1f0Ni10";
       };
       lingyu = {
+        name = "lingyu";
         isNormalUser = true;
         hashedPassword = "$y$j9T$HZvnP.0ZR5uBiDAviT9xA.$MSExGgePZwjIDZq2n3fOUGGguWKEgvjuIKImW4uf7p4";
         linger = true;
@@ -83,6 +85,7 @@
           (lib.optionals config.services.seatd.enable [ config.services.seatd.group ])
         ];
 	      home = "/home/lingyu";
+        uid = 1000;
       };
     };
   };
