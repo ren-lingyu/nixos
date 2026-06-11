f80fc86fea0db835044444429348d45ca80efcdf
ren-lingyu <Ren_Lingyu@outlook.com>
2026-06-11 14:50:07 +0800

    style(editor): fix indentation of hm module config block
    - 修正 `lib.mkIf` 块内代码缩进.

--- modules/features/editor/hm/default.nix
+++ modules/features/editor/hm/default.nix
@@ -2,95 +2,95 @@
 
   config = lib.mkIf osConfig.modules.features.editor.enable {
     
-  programs.nixvim = {
-    enable = osConfig.modules.features.editor.neovim.enable;
-    defaultEditor = osConfig.modules.features.editor.defaultEditor == "neovim";
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    opts = {
-      number = true;
-      relativenumber = true;
-      expandtab = true;
-      tabstop = 2;
-      shiftwidth = 2;
-      softtabstop = 2;
-      mouse = "a";
-      ignorecase = true;
-      smartcase = true;
-      termguicolors = true;
-      clipboard = "unnamedplus";
-    };
-    plugins = {
-      lualine.enable = true;
-      gitsigns.enable = true;
-      which-key.enable = true;
-      telescope.enable = true;
-      web-devicons.enable = true;
-      mini-icons.enable = true;
-      cmp = {
-        enable = true;
-        autoEnableSources = true;
+    programs.nixvim = {
+      enable = osConfig.modules.features.editor.neovim.enable;
+      defaultEditor = osConfig.modules.features.editor.defaultEditor == "neovim";
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      opts = {
+        number = true;
+        relativenumber = true;
+        expandtab = true;
+        tabstop = 2;
+        shiftwidth = 2;
+        softtabstop = 2;
+        mouse = "a";
+        ignorecase = true;
+        smartcase = true;
+        termguicolors = true;
+        clipboard = "unnamedplus";
       };
-      treesitter = {
-        enable = true;
-        autoLoad = true;
-        nixvimInjections = true;
-        highlight.enable = true;
-        indent.enable = true;
-        folding.enable = true;
-        nixGrammars = true;
+      plugins = {
+        lualine.enable = true;
+        gitsigns.enable = true;
+        which-key.enable = true;
+        telescope.enable = true;
+        web-devicons.enable = true;
+        mini-icons.enable = true;
+        cmp = {
+          enable = true;
+          autoEnableSources = true;
+        };
+        treesitter = {
+          enable = true;
+          autoLoad = true;
+          nixvimInjections = true;
+          highlight.enable = true;
+          indent.enable = true;
+          folding.enable = true;
+          nixGrammars = true;
+        };
+        lsp = {
+          enable = true;
+          servers = {
+            nixd.enable = true;
+            bashls.enable = true;
+            clangd.enable = true;
+            gopls.enable = true;
+            jdtls.enable = true;
+            ts_ls.enable = true;
+            pyright.enable = true;
+            lua_ls.enable = true;
+            marksman.enable = true;
+            yamlls.enable = true;
+          };
+        };
       };
-      lsp = {
-        enable = true;
-        servers = {
-          nixd.enable = true;
-          bashls.enable = true;
-          clangd.enable = true;
-          gopls.enable = true;
-          jdtls.enable = true;
-          ts_ls.enable = true;
-          pyright.enable = true;
-          lua_ls.enable = true;
-          marksman.enable = true;
-          yamlls.enable = true;
+      colorschemes = {
+        catppuccin = {
+          enable = true;
+          settings.flavour = "mocha";
         };
       };
     };
-    colorschemes = {
-      catppuccin = {
+
+    programs.vim = {
+      enable = osConfig.modules.features.editor.vim.enable;
+      package = pkgs.vim;
+      defaultEditor = osConfig.modules.features.editor.defaultEditor == "vim";
+    };
+
+    programs.emacs = {
+      enable = osConfig.modules.features.editor.emacs.enable;
+      package = pkgs.emacs-gtk;
+    };
+
+    services.emacs = {
+      enable = config.programs.emacs.enable;
+      package = pkgs.emacs-gtk;
+      extraOptions = [ "--debug-init" ];
+      defaultEditor = osConfig.modules.features.editor.defaultEditor == "emacs";
+      socketActivation = {
         enable = true;
-        settings.flavour = "mocha";
+      };
+      startWithUserSession = false;
+      client = {
+        enable = false;
+        arguments = ["-c"];
       };
     };
-  };
-
-  programs.vim = {
-    enable = osConfig.modules.features.editor.vim.enable;
-    package = pkgs.vim;
-    defaultEditor = osConfig.modules.features.editor.defaultEditor == "vim";
-  };
-
-  programs.emacs = {
-    enable = osConfig.modules.features.editor.emacs.enable;
-    package = pkgs.emacs-gtk;
-  };
-
-  services.emacs = {
-    enable = config.programs.emacs.enable;
-    package = pkgs.emacs-gtk;
-    extraOptions = [ "--debug-init" ];
-    defaultEditor = osConfig.modules.features.editor.defaultEditor == "emacs";
-    socketActivation = {
-      enable = true;
-    };
-    startWithUserSession = false;
-    client = {
-      enable = false;
-      arguments = ["-c"];
-    };
-  };
-  
+    
   };
   
 }
