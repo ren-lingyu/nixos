85cbac6f598ccf6ce2b41cb208fd871e69679a9e
ren-lingyu <Ren_Lingyu@outlook.com>
2026-06-11 15:14:17 +0800

    refactor(lingyu): extract home configuration into home.nix
    - 将 `home`, `systemd`, `programs.direnv`, `programs.ssh`, `programs.zathura`, `xdg` 配置从 `modules/users/lingyu/default.nix` 移动至新文件 `home.nix`.
    - 在 `default.nix` 的 `imports` 中新增 `./home.nix` 引用.

---
+++ modules/users/lingyu/home.nix
@@ -0,0 +1,102 @@
+{ config, lib, pkgs, ... } : {
+  
+  home = {
+    packages = with pkgs; (builtins.concatLists [
+      [ jq ripgrep unzip trash-cli tree ]
+      [ htop pciutils tcpdump mtr nmap netcat socat dnsutils ]
+      [ xclock xeyes xclip ]
+      [ gnumake ]
+      [ chafa ]
+      [ vulkan-tools ]
+      [ adwaita-icon-theme xcursor-themes ]
+    ]);
+    sessionVariables = {
+      QT_QPA_PLATFORM = "xcb";
+      XCURSOR_THEME = "Adwaita";
+      XCURSOR_SIZE = "24";
+      GPG_TTY = "$(tty)";
+      TEXINPUTS = "$HOME/org/texmf//:";
+      BIBINPUTS = "$HOME/org/texmf//:";
+      BSTINPUTS = "$HOME/org/texmf//:";
+    };
+    sessionPath = [
+      "${config.xdg.binHome}"
+    ];
+    file = {
+      ".local/bin/rc" = {
+        enable = true;
+        source = ./.local/bin/rc.sh;
+        target = ".local/bin/rc";
+        executable = true;
+      };
+      ".local/bin/org" = {
+        enable = true;
+        source = ./.local/bin/org.sh;
+        target = ".local/bin/org";
+        executable = true;
+      };
+      ".local/bin/ssha" = {
+        enable = true;
+        source = ./.local/bin/ssha.sh;
+        target = ".local/bin/ssha";
+        executable = true;
+      };
+    };
+  };
+
+  systemd = {
+    user = {
+      sessionVariables = config.home.sessionVariables;
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    package = pkgs.direnv;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings = {
+      "*" = {
+        forwardAgent = false;
+      	serverAliveInterval = 0;
+      	serverAliveCountMax = 3;
+      	compression = false;
+      	addKeysToAgent = "no";
+      	hashKnownHosts = false;
+      	userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
+      	controlMaster = "no";
+      	controlPath = "${config.home.homeDirectory}/.ssh/master-%r@%n:%p";
+      	controlPersist = "no";
+      };
+    };
+  };
+
+  programs.zathura = {
+    enable = true;
+    package = pkgs.zathura;
+    options = {};
+    mappings = {};
+  };
+
+  xdg = {
+    enable = true;
+    cacheHome = "${config.home.homeDirectory}/.cache";
+    configHome = "${config.home.homeDirectory}/.config";
+    binHome = "${config.home.homeDirectory}/.local/bin";
+    dataHome = "${config.home.homeDirectory}/.local/share";
+    stateHome = "${config.home.homeDirectory}/.local/state";
+    autostart.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = "org.pwmt.zathura.desktop";
+      };
+    };
+  };
+  
+}
