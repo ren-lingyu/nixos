2bddabd86fa242b1e9d922a124a893c395b0a190
ren-lingyu <Ren_Lingyu@outlook.com>
2026-08-05 14:39:25 +0800

    docs(shell): fill in package option descriptions and messages
    - 为 `zsh.package` 和 `bash.package` 补充 `description`, `example` 和 `unique` 冲突消息.

--- modules/features/shell/extra-options.nix
+++ modules/features/shell/extra-options.nix
@@ -3,22 +3,24 @@ feature_ : { config, pkgs, lib, llib, ... } : {
   zsh = {
     package = lib.mkOption {
       type = lib.types.unique {
-        message = "";
+        message = "Conflicting definitions for `modules.features.${feature_}.zsh.package`.";
       } lib.types.package;
       internal = true;
       default = pkgs.zsh;
-      description = "";
+      example = lib.literalExpression "pkgs.zsh";
+      description = "Zsh package shared by the NixOS and Home Manager shell configurations.";
     };
   };
 
   bash = {
     package = lib.mkOption {
       type = lib.types.unique {
-        message = "";
+        message = "Conflicting definitions for `modules.features.${feature_}.bash.package`.";
       } lib.types.package;
       internal = true;
       default = pkgs.bash;
-      description = "";
+      example = lib.literalExpression "pkgs.bashInteractive";
+      description = "Bash package shared by the NixOS and Home Manager shell configurations.";
     };
   };
 
