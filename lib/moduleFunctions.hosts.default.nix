e9979332f891b8ae4bd8e9e265ec5e2f0c01d813
ren-lingyu <Ren_Lingyu@outlook.com>
2026-08-20 03:34:17 +0800

    feat(lib): add WireGuard networking builder
    - 在 `moduleFunctions.hosts.default` 中新增 `mkWireGuardNetworking`，根据 host registry 生成 hub/spoke 接口和 peer 配置.
    - 通过注入的地址与接口命名规则派生私网配置，并设置 hub 监听端口、防火墙和 spoke keepalive.
    - 在当前 host 未声明 WireGuard metadata 时返回空配置.

--- lib/moduleFunctions.hosts.default.nix
+++ lib/moduleFunctions.hosts.default.nix
@@ -1,4 +1,4 @@
-{ lib } : {
+{ lib } : rec {
 
   getUniqueEnabledHost = hosts_ : let
     enabledHostValues_ = builtins.attrValues (
@@ -11,6 +11,144 @@
     if (builtins.length enabledHostValues_) == 1
     then builtins.head enabledHostValues_
     else {}
+  );
+
+  mkWireGuardNetworking = { registry, privateKeyFile, wgIpRule, wgNameRule } : let
+
+    hosts_ = registry;
+
+    privateKeyFile_ = privateKeyFile;
+
+    wgIpRule_ = wgIpRule;
+
+    wgNameRule_ = wgNameRule;
+
+  in let
+
+    enabledHost_ = getUniqueEnabledHost hosts_;
+
+    wireguardHosts_ = (lib.filterAttrs
+      (unused_name_ : host_ : host_.wireguard != null)
+      hosts_
+    );
+
+    hubHosts_ = (lib.filterAttrs
+      (unused_name_ : host_ : host_.wireguard.endpoint != null)
+      wireguardHosts_
+    );
+
+    otherWireguardHosts_ = (lib.filterAttrs
+      (unused_name_ : host_ : !(host_.enable or false))
+      wireguardHosts_
+    );
+
+    interfaceNameOf_ = hubHost_ : wgNameRule_ hubHost_.number;
+
+    intraIpAddressOf_ = hubHost_ : host_ : wgIpRule_ hubHost_.number host_.number;
+
+    endpointOf_ = host_ : let
+      endpoint_ = host_.wireguard.endpoint;
+      address_ = (
+        if (builtins.match ".*:.*" endpoint_.address) != null
+        then "[${endpoint_.address}]"
+        else endpoint_.address
+      );
+    in "${address_}:${builtins.toString endpoint_.port}";
+
+    peerOf_ = hubHost_ : host_ : (lib.mergeAttrsList
+      [
+
+        ({
+          publicKey = host_.wireguard.publicKey;
+          allowedIPs = [
+            "${intraIpAddressOf_ hubHost_ host_}/32"
+          ];
+        })
+
+        (lib.optionalAttrs
+          (host_.number == hubHost_.number)
+          {
+            endpoint = endpointOf_ hubHost_;
+            persistentKeepalive = 25;
+          }
+        )
+
+      ]
+    );
+
+    interfaceOf_ = hubHost_ : peerHosts_ : (lib.mergeAttrsList
+      [
+
+        ({
+          type = "wireguard";
+          ips = [
+            "${intraIpAddressOf_ hubHost_ enabledHost_}/24"
+          ];
+          privateKeyFile = privateKeyFile_;
+          generatePrivateKeyFile = false;
+          peers = builtins.map
+            (host_ : peerOf_ hubHost_ host_)
+            peerHosts_;
+        })
+
+        (lib.optionalAttrs
+          (builtins.all
+            (x_ : x_)
+            [
+              (enabledHost_.number == hubHost_.number)
+              (enabledHost_.wireguard.listenPort != null)
+            ]
+          )
+          {
+            listenPort = enabledHost_.wireguard.listenPort;
+          }
+        )
+
+      ]
+    );
+
+    interfaces_ = (lib.mapAttrs'
+      (unused_name_ : hubHost_ : {
+        name = interfaceNameOf_ hubHost_;
+        value = (interfaceOf_
+          hubHost_
+          (
+            if enabledHost_.number == hubHost_.number
+            then builtins.attrValues otherWireguardHosts_
+            else [
+              hubHost_
+            ]
+          )
+        );
+      })
+      hubHosts_
+    );
+
+  in (lib.optionalAttrs
+    (enabledHost_.wireguard != null)
+    {
+
+      wireguard = {
+        enable = true;
+        interfaces = interfaces_;
+      };
+
+      firewall = (lib.optionalAttrs
+        (builtins.all
+          (x_ : x_)
+          [
+            (enabledHost_.wireguard.endpoint != null)
+            (enabledHost_.wireguard.listenPort != null)
+          ]
+        )
+        {
+          allowedUDPPorts = [
+            enabledHost_.wireguard.listenPort
+          ];
+        }
+      );
+
+    }
   );
 
 }
